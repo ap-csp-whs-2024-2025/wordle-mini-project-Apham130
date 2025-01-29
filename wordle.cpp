f2c23cc2a903afeb252dd06689091d1df8293e1f
Apham130 <andypham4747@gmail.com>
2025-01-29 20:58:46 +0000

    done with main assignment, gonna do extensions now or maybe another day who knows lalalala

--- wordle.cpp
+++ wordle.cpp
@@ -19,24 +19,35 @@ std::vector<int> createSecret()
 std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess) 
 {   
     std::vector<std::string> hint = {};
-    for (int counter = 1; counter < 5; counter = counter + 1)
+    for (int counter = 0; counter < 4; counter = counter + 1)
     {
-        if (secret[couter] == guess[counter])
+        if (secret[counter] == guess[counter])
         {
-            append(hint, O)
+            hint.push_back("O");
         } else
         {
-            append(hint, X)
+            hint.push_back("X");
         } 
     }
 
-    return {bool1, bool2, bool3, bool4};  
+    return hint;  
 }
 
-bool winGame(std::vector<int> secret, std::vector<int> guess) 
+bool winGame(std::vector<std::string> guess) 
 {
-
-    return false;  
+    int numCorrect = 0;
+    for (int counter = 0; counter < length(guess); counter = counter + 1)
+    {
+        if (guess[counter] == "O");
+        {
+            numCorrect = numCorrect + 1;
+        }
+    } 
+    if (numCorrect == 4)
+    {
+        return true;
+    }
+    return false;
 }
 
 int main()
@@ -51,24 +62,26 @@ int main()
     int num_guesses = 0;
 
     display(secret_code);
-    std::cout << "Welcome to Number Wordle!\n";
+    std::cout << "Welcome to Number Wordle!\nGuess the 4 digit number one digit at a time!\n";
     
-    while (!winGame(secret_code, user_guess))   
+    while (!winGame(hint))   
     {
+        num_guesses = num_guesses + 1;
         std::cout << "\nGuess #" << num_guesses << ": ";
         hint = {};
+        user_guess = {};
         for (int counter = 0; counter < secret_code_length; counter = counter + 1)
         {
             int input;
             std::cin >> input;
             user_guess.push_back(input);   
         }
-
         hint = getHint(secret_code, user_guess);
         display(hint);
-        num_guesses = num_guesses + 1;
+        // display(secret_code);
+        // display(user_guess);
     }
 
-    // std::cout << "Congrats!  You guessed the code!  It took you " << num_guesses << " tries.\n";
+    std::cout << "Congrats!  You guessed the code!  It took you " << num_guesses << " tries.\n";
     return 0;
 }
